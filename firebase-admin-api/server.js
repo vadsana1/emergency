0799620737bac4a85c6544cb2719d6e994f8c050
vadsana <vadsanasvnl@gmail.com>
2025-06-21 21:57:20 +0700

    Initial clean commit

---
+++ firebase-admin-api/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const admin = require('firebase-admin');
+
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// import และใช้ route ที่แยกไว้
+app.use(require('./routes/createUser')(admin));
+app.use(require('./routes/editUser')(admin));
+app.use(require('./routes/deleteUser')(admin));
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Admin API running on http://localhost:${PORT}`);
+});
